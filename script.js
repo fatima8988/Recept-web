5aa178b3400c4de7556337e564d74fa8ce390d5f
fatima8988 <fatima.chohan@elev.ga.lbs.se>
2026-04-07 19:13:15 +0200

    2

---
+++ script.js
@@ -0,0 +1,16 @@
+const items = document.querySelectorAll(".card");
+const filters = document.querySelectorAll(".dropdown-menu p");
+
+filters.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const value = btn.dataset.filter;
+
+    items.forEach(card => {
+      if (value === "all" || card.dataset.category === value) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
