51958794a689c7f5740ae84edde7c36a5bac6857
fatima8988 <fatima.chohan@elev.ga.lbs.se>
2026-04-08 21:30:38 +0200

    8

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+const heroImage = document.getElementById("heroImage");
+
+const images = [
+  "https://images.unsplash.com/photo-1578985545062-69928b1d9587",
+  "https://images.unsplash.com/photo-1606312619070-d48b4c652a52",
+  "https://images.unsplash.com/photo-1565958011703-44f9829ba187",
+  "https://images.unsplash.com/photo-1589308078059-be1415eab4c3"
+];
+
+let index = 0;
+
+setInterval(() => {
+  // fade out
+  heroImage.style.opacity = 0;
+
+  setTimeout(() => {
+    index = (index + 1) % images.length;
+    heroImage.src = images[index];
+
+    // fade in
+    heroImage.style.opacity = 1;
+  }, 500);
+
+}, 2000); // ändra till 1000 om du vill snabbare
